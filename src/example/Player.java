bb22fbc0e1024582886023c8534d9264d6f181c2
Miknielsen <miknie13@student.aau.dk>
2015-03-11 13:55:21 +0100

    Revert "Revert "Creation of""
    This reverts commit 616cc496dfa52c5c2170b603085a4951e303bfb6.

---
+++ src/example/Player.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Player {
+    
+    //Create player - Rectangle
+    
+    
+    //Fixiate player on the y-axis
+    //Allow player to move on the x-axis with the arrow-keys or mouse
+    
+}
