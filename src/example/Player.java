33cf8090c5f0ea17e63b52e3bf0cca26e80defc3
Nicolai Foldager <nfoldager0@gmail.com>
2015-03-30 15:05:01 +0200

    Started player Movement method in Player Class

--- src/example/Player.java
+++ src/example/Player.java
@@ -1,6 +1,22 @@
 package example;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.util.Log;
+
 public class Player {
+        
+    
+    
+    public void Movement(GameContainer key) {
+        
+        Input input = key.getInput();
+        if(input.isKeyPressed(Input.KEY_D)) {
+            
+            Log.debug("Game terminated due to ESC being pressed!");
+        }
+        
+    }
     
     //Create player - Rectangle
     
