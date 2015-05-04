ba0ac03f417dfb17fb99d9ee9429f1a9fbbe924a
Nicolai Foldager <nfoldager0@gmail.com>
2015-05-04 10:07:48 +0200

    Cleaned conflicts for Mikkel <3

--- src/example/Ball.java
+++ src/example/Ball.java
@@ -1,28 +1,5 @@
 package example;
 
-<<<<<<< HEAD
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.util.Log;
-
-public class Ball {
-    
-    public float 
-    ballRadius = 15.0f,                                                             //The radius of the ball
-    ballXpos = SimpleSlickGame.getScreenWorkingWidth()/2.0f,                        //The start X-coordinates of the ball, center of screen
-    ballYpos = SimpleSlickGame.getScreenWorkingHeight()*0.80f;                      //The start Y-coordinates of the ball, 80% on the Y-axis
-    
-    public void createBall(Graphics g){
-
-        g.drawOval(ballXpos, ballYpos, ballRadius, ballRadius);                     //Creates an oval shape, X-pos, Y-pos, radius, radius
-    }
-    
-    public void ballMovement(){
-        
-    }
-
-=======
 import org.newdawn.slick.Graphics;
 
 public class Ball {
@@ -83,5 +60,4 @@ public class Ball {
 		speedY = speedY * -1;
 	}
 	
->>>>>>> origin/master
 }
