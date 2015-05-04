def839b722a2bc59e6e52dbb2c82eea3e39325b8
madskondrup <mkondrup@hotmail.dk>
2015-05-04 13:41:40 +0200

    fixed an unused import.

--- src/example/Brick.java
+++ src/example/Brick.java
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
+
 package example;
 
 import java.util.Random;
 
 import org.newdawn.slick.Graphics;
 
-public class Brick {
-	
-	public Brick(Graphics g, int xPos, int yPos, int x, int y){
-		g.drawRect(xPos,yPos, x, y);
-	}
-	
-}
-=======
-package example;
- 
 import java.util.Random;
  
 import org.newdawn.slick.Graphics;
@@ -26,4 +16,3 @@ public class Brick {
         }
        
 }
->>>>>>> origin/master
