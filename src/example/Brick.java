0519dfe09a09519b1b9af6db8a27ddbc6c1e17f8
Miknielsen <miknie13@student.aau.dk>
2015-05-05 13:09:23 +0200

    Added JavaDoc to Brick

--- src/example/Brick.java
+++ src/example/Brick.java
@@ -9,7 +9,14 @@ public class Brick {
 	int g_yPos;
 	int length = 55;
 	int height = 15;
-       
+ 
+	/**
+	 * Creates an enemy brick at a given position and given size
+	 * 
+	 * @param g        Graphics engine to draw the brick
+	 * @param xPos     Integer value which sets the x-coordinate of a brick
+	 * @param yPos     Integer value which sets the y-coordinate of a brick
+	 */
     public Brick(Graphics g, int xPos, int yPos){
             g.drawRect(xPos,yPos, length, height);
           
@@ -17,26 +24,56 @@ public class Brick {
             g_yPos = yPos;
     }
     
+    /**
+     * Setter for the position of the brick on the x-axis
+     * 
+     * @param pos       Integer value to set the x-coordinates of a brick
+     */
     public void setXpos(int pos) {
     	g_xPos = pos;
     }
     
+    /**
+     * Setter for the position of the brick on the y-axis
+     * 
+     * @param pos       Integer value to set the y-coordinates of a brick
+     */
     public void setYpos(int pos) {
     	g_yPos = pos;
     }
     
+    /**
+     * Getter for the position that has been set for a brick on the x-axis
+     * 
+     * @return          Returns the integer value of the coordinate of a brick on the x-axis
+     */
     public int getXpos() {
     	return g_xPos;
     }
     
+    /**
+     * Getter for the position that has been set for a brick on the y-axis
+     * 
+     * @return          Returns the integer value of the coordinate of a brick on the y-axis
+     */
     public int getYpos() {
     	return g_yPos;
     }
     
+    /**
+     * Getter for the length of a brick
+     * 
+     * @return          Returns the integer value of the length of a brick
+     */
     public int getLength() {
     	return length;
     }
     
+    /**
+     * Getter for the height of a brick
+     * 
+     * @return          Returns the integer value of the height of a brick
+     */
     public int getHeight() {
     	return height;
     }
