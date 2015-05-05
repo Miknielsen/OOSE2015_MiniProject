e44e5725afbfb5a13e3c00440a616184c8bb22cd
Miknielsen <miknie13@student.aau.dk>
2015-05-05 13:01:01 +0200

    Added JavaDoc to RandomGen

--- src/example/RandomGen.java
+++ src/example/RandomGen.java
@@ -4,6 +4,13 @@ import java.util.Random;
 
 public class RandomGen {
 
+    /**
+     * Creates a random value between 2 given input values, minimum and maximum
+     * 
+     * @param min       Takes an integer as value and sets it to minimum
+     * @param max       Takes an integer as value and sets it to maximum
+     * @return          Returns and integer which is the random value between minimum and maximum
+     */
 	public static int randInt(int min, int max) {
 		
 		Random rand = new Random();
