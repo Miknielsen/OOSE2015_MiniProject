ae883f4bdfb88ff459fa8eac5b24c9526cda6e16
Nicolai Foldager <nfoldager0@gmail.com>
2015-05-05 11:55:53 +0200

    Removed unused library

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -14,9 +14,6 @@ package example;
 import java.util.logging.Level;
 import java.util.logging.Logger;
  
-
-
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import org.newdawn.slick.AppGameContainer;
