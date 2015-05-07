7e6d63501fcb7458d2c56fd0847f0febef5c9f89
Miknielsen <miknie13@student.aau.dk>
2015-05-07 10:33:58 +0200

    Removed another unused library in simpleSLickGame

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -18,7 +18,6 @@ import javax.swing.JOptionPane;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
